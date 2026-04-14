15e6070ebae1907656e73ec93006159ec52bc542
Syrnikvlad777228 <dale2010.04.07@gmail.com>
2026-04-14 17:11:09 +0300

    Update note_rep.dart

--- lib/servies/note_rep.dart
+++ lib/servies/note_rep.dart
@@ -3,8 +3,6 @@ import 'dart:convert';
 import 'package:path/path.dart' as p;
 import '../models/note.dart';
 
-// 1. Абстрактный класс (Интерфейс)
-// Здесь перечислены все функции, которые должны быть в репозитории
 abstract class Rep {
   void add(Note note);
   void del(int id);
@@ -16,26 +14,21 @@ abstract class Rep {
   List<Note> sorttitle();
 }
 
-// 2. Реализация репозитория
 class Repository extends Rep {
   late final Directory _storageDir;
 
   Repository() {
-    // Получаем путь к папке пользователя (C:\Users\Имя)
     final userHome = Platform.environment['USERPROFILE'] ?? Directory.current.path;
     
-    // Собираем путь: Документы -> MyNotes (используем только английские буквы)
     final String pathToDocs = p.join(userHome, 'Documents', 'MyNotes');
     
     _storageDir = Directory(pathToDocs);
-    
-    // Создаем папку, если её еще нет
+
     if (!_storageDir.existsSync()) {
       _storageDir.createSync(recursive: true);
     }
   }
 
-  // Внутренний метод для записи файла на диск
   void _saveToFile(Note note) {
     try {
       final filePath = p.join(_storageDir.path, '${note.id}.json');
@@ -55,7 +48,7 @@ class Repository extends Rep {
 
   @override
   void upd(Note note) {
-    _saveToFile(note); // В пофайловой логике обновление — это перезапись
+    _saveToFile(note); 
     print('Система: Заметка ${note.id} обновлена.');
   }
 
@@ -74,7 +67,6 @@ class Repository extends Rep {
   List<Note> all() {
     if (!_storageDir.existsSync()) return [];
 
-    // Читаем все JSON файлы из папки и превращаем их в объекты Note
     return _storageDir
         .listSync()
         .whereType<File>()
@@ -98,14 +90,12 @@ class Repository extends Rep {
 
   @override
   List<Note> findTag(String tag) {
-    // Ищем заметки, у которых в списке тегов есть нужный нам
     return all().where((n) => n.tags.any((t) => t.toLowerCase() == tag.toLowerCase())).toList();
   }
 
   @override
   List<Note> sortdata() {
     final notes = all();
-    // Сортировка: новые даты сверху
     notes.sort((a, b) => b.createdAt.compareTo(a.createdAt));
     return notes;
   }
@@ -113,7 +103,6 @@ class Repository extends Rep {
   @override
   List<Note> sorttitle() {
     final notes = all();
-    // Сортировка по алфавиту (без учета регистра)
     notes.sort((a, b) => a.title.toLowerCase().compareTo(b.title.toLowerCase()));
     return notes;
   }
